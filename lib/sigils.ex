d778f0fbe2c6d2a79999038dff60feb031cb076b
HoffsMH <hecker.matthew@gmail.com>
2018-02-04 13:49:25 -0500

    found a neat little toy apparently you can affect the spacing of the multiline quote by changing the indenting of the closing quote. Basically you want the closing quote to be at the "base" of the strings

--- lib/sigils.ex
+++ lib/sigils.ex
@@ -24,4 +24,58 @@ defmodule Sigils do
   def c_sigil do
     ~c(hiiii)
   end
+
+  @doc "
+
+  "
+  def w_sigil do
+    ~w(foo bar bat)
+  end
+
+  # playing around with this
+  # https://stackoverflow.com/questions/42014121/elixir-heredocs-closing-triple-quotes-behavior
+  @doc """
+    iex> import Sigils
+    iex> multiline_quotes_1()
+    "hi\\nthere\\n  indented\\nwhat\\n"
+  """
+  def multiline_quotes_1 do
+    var = """
+    hi
+    there
+      indented
+    what
+    """
+    var
+  end
+
+  @doc """
+    iex> import Sigils
+    iex> multiline_quotes_2()
+    "hi\\nthere\\nindented\\nwhat\\n"
+  """
+  def multiline_quotes_2 do
+    var = """
+    hi
+    there
+      indented
+    what
+      """
+    var
+  end
+
+  @doc """
+    iex> import Sigils
+    iex> multiline_quotes_3()
+    "  hi\\n  there\\n    indented\\n  what\\n"
+  """
+  def multiline_quotes_3 do
+    var = """
+    hi
+    there
+      indented
+    what
+  """
+    var
+  end
 end
